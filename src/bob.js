88eac2bdc20e11ed065cd7384f2914d40f46545f
PensoGlide <eduardo.augusto.santiago@gmail.com>
2022-08-13 23:41:10 +0100

    Changed bob.js for ERC20

--- src/bob.js
+++ src/bob.js
@@ -1,16 +1,20 @@
 (async () => {
-    const ethers = require('ethers')
-    const zksync = require('zksync')
-    const utils = require('./utils')
+    const ethers = require('ethers');
+    const zksync = require('zksync');
+    const utils = require('./utils');
+    const token = 'USDT';
+
+    const SLEEP_INTERVAL = process.env.SLEEP_INTERVAL || 5000;
   
     const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME); // export NETWORK_NAME=rinkeby
     const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME);
 
     const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_KEY, ethersProvider);
+    const tokenSet = zkSyncProvider.tokenSet;
     console.log(`Bob's Rinkeby address is: ${bobRinkebyWallet.address}`);
-    console.log(`Bob's initial balance on Rinkeby is: ${ethers.utils.formatEther(await bobRinkebyWallet.getBalance())}`);
 
     const bobZkSyncWallet = await utils.initAccount(bobRinkebyWallet, zkSyncProvider, zksync);
+    console.log(`Bob's initial balance on Rinkeby is: ${await bobZkSyncWallet.getEthereumBalance(token)}`)
 
     process.on('SIGINT', () => {
         console.log('Disconnecting')
@@ -18,7 +22,7 @@
         process.exit()
     })
     setInterval(async () => {
-        await utils.displayZkSyncBalance(bobZkSyncWallet, ethers);
+        await utils.displayZkSyncBalance(bobZkSyncWallet, tokenSet);
         console.log('---')
     }, SLEEP_INTERVAL)
 })()
